536e4ab9aa25c050c36958c92bddde11a8e60b1f
Kirti Girdhar <kirtigirdhar2004@gmail.com>
2026-05-04 13:54:09 +0530

    Update TaskForm with attachment field support

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,15 +1,16 @@
 from django import forms
 from django.utils import timezone
-from .models import Task
+from tasks.models import Task
 
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'status', 'priority', 'due_date', 'project', 'assigned_to']
-    
+    # cleared_data() - means it convert the input from the user into python data types
+    #  such as text to string, IntegerField to int, DateField to datetime.date object,etc.
     def clean_due_date(self):
         due_date = self.cleaned_data.get('due_date')
-        if due_date and due_date <= timezone.now():
-            raise forms.ValidationError("Due date must be greater than the current date.")
+        if due_date < timezone.now():
+            raise forms.ValidationError("Due date must be a future date.")
         return due_date
