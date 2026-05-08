5e248dad36d20cbd5ae3da1e2254743de6996e27
Kirti Girdhar <kirtigirdhar2004@gmail.com>
2026-05-08 11:34:05 +0530

    Build Serializers

---
+++ task_manager/tasks/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from tasks.models import Task
+from users.models import CustomUser
+
+class TestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+        read_only_fields= ['assigned_to']
+
+    def create(self, validated_data):
+        request= self.context.get('request')
+        user= request.user if request else None
+        # user= CustomUser.objects.first()
+
+        task= Task.objects.create(
+            assigned_to= user,
+            **validated_data
+        )
+        return task
+        
