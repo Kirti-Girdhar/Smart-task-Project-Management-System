1a169722e617f02b3f48abc5607a55af08f01a03
Kirti Girdhar <kirtigirdhar2004@gmail.com>
2026-05-08 11:31:33 +0530

    Url for tokens

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -15,12 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('tasks.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
 
 # this is to access media files during development 
