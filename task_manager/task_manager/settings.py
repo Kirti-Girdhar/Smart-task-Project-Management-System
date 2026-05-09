231783d34b1739d05f718855f2e765df99b9adbb
Kirti Girdhar <kirtigirdhar2004@gmail.com>
2026-05-09 12:55:14 +0530

    Configure DRF with django_filters, pagination, and authentication

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -47,12 +47,22 @@ INSTALLED_APPS = [
     'tasks',
     'comments',
     'rest_framework',
+    'django_filters',
 ]
 
 REST_FRAMEWORK= {
     'DEFAULT_AUTHENTICATION_CLASSES':(
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    
+    'DEFAULT_FILTER_BACKENDS': (
+    'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+
+    'PAGE_SIZE': 5,
 }
 
 MIDDLEWARE = [
