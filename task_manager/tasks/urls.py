4f2518d2f990ef92f2f6984e40f6569c5f0d2c21
Kirti Girdhar <kirtigirdhar2004@gmail.com>
2026-05-09 12:55:48 +0530

    Refactor URLs to use DRF routers instead of function-based views

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from tasks.views import task_list,task_detail
+# from tasks.views import task_list,task_detail
+from rest_framework.routers import DefaultRouter
+from .views import TaskViewSet
 
-urlpatterns=[
-    path('tasks/', task_list),
-    path('tasks/<int:id>/', task_detail),
-]
+
+# urlpatterns=[
+#     path('tasks/', task_list),
+#     path('tasks/<int:id>/', task_detail),
+# ]
+
+router= DefaultRouter()
+router.register('tasks', TaskViewSet)
+urlpatterns= router.urls
