acbb2568d6ea56067f7a275640d2db0c0ada6bb8
Kirti Girdhar <kirtigirdhar2004@gmail.com>
2026-05-10 12:25:47 +0530

    Update tasks serializers

--- task_manager/tasks/serializers.py
+++ task_manager/tasks/serializers.py
@@ -1,21 +1,24 @@
 from rest_framework import serializers
 from tasks.models import Task
 from users.models import CustomUser
+from users.serializers import UserSerializer
 
 class TaskSerializer(serializers.ModelSerializer):
+
+    assigned_to = UserSerializer(read_only= True)
     class Meta:
         model = Task
         fields = '__all__'
         read_only_fields= ['assigned_to']
 
-    def create(self, validated_data):
-        request= self.context.get('request')
-        user= request.user if request else None
-        # user= CustomUser.objects.first()
+    # def create(self, validated_data):
+    #     request= self.context.get('request')
+    #     user= request.user if request else None
+    #     # user= CustomUser.objects.first()
 
-        task= Task.objects.create(
-            assigned_to= user,
-            **validated_data
-        )
-        return task
+    #     task= Task.objects.create(
+    #         assigned_to= user,
+    #         **validated_data
+    #     )
+    #     return task
         
