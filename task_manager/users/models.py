2a31e1a1770d1bce3d3b46573155109441098fe2
Kirti Girdhar <kirtigirdhar2004@gmail.com>
2026-05-11 12:43:54 +0530

    Update users model

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -6,9 +6,9 @@ class CustomUser(AbstractUser):
     ROLE_CHOICES = (
         ('admin', 'Admin'),
         ('manager', 'Manager'),
-        ('user', 'User'),)
+        ('employee', 'Employee'),)
     phone_number = models.CharField(max_length=15,blank=True)
-    role = models.CharField(max_length=10,choices=ROLE_CHOICES,default='user')
+    role = models.CharField(max_length=20,choices=ROLE_CHOICES,default='employee')
     profile_image = models.ImageField(upload_to='profiles/',null=True,blank=True)
 
     def __str__(self):
