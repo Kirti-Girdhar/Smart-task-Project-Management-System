9ed97d41d4baa954d920e0eb5866112de1177c35
Kirti Girdhar <kirtigirdhar2004@gmail.com>
2026-05-11 12:44:11 +0530

    Update users admin

--- task_manager/users/admin.py
+++ task_manager/users/admin.py
@@ -1,5 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 # Register your models here.
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+
+        ('Additional Info', {
+            'fields': (
+                'role',
+                'phone_number',
+                'profile_image',
+            )
+        }),
+
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+
+        ('Additional Info', {
+            'fields': (
+                'role',
+                'phone_number',
+                'profile_image',
+            )
+        }),
+
+    )
+
+    list_display = (
+        'username',
+        'email',
+        'role',
+        'phone_number',
+        'is_staff',
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
