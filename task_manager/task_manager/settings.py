f428e556ea6a47d0eaf9a901272cd9c2455e2866
Kirti Girdhar <kirtigirdhar2004@gmail.com>
2026-05-29 11:22:10 +0530

    Clean up settings.py formatting
    - Remove unnecessary blank line before INSTALLED_APPS
    - Improve code readability

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','127.0.0.1,localhost').split(',')
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
