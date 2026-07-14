117761be4b4080e4916393c6ec7b300a8413f89b
Kirti Girdhar <kirtigirdhar2004@gmail.com>
2026-07-14 10:06:44 +0530

    Update production settings

--- task_manager/task_manager/settings/production.py
+++ task_manager/task_manager/settings/production.py
@@ -3,3 +3,15 @@ from .base import *
 DEBUG= False
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','127.0.0.1,localhost').split(',')
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_BROWSER_XSS_FILTER = True
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+X_FRAME_OPTIONS = "DENY"
