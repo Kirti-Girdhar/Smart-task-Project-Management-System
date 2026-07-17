707f765ea2c5201a665fd8cafb80e91bcca5d571
Kirti Girdhar <kirtigirdhar2004@gmail.com>
2026-07-17 10:09:06 +0530

    Register projects API routes in root URLs

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tasks.urls')),
     path('api/', include('comments.urls')),
+    path('api/', include('projects.urls')),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
