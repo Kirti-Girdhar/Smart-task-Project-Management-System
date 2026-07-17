06067d2c2f30fbd7664219a7c8b876461f17d71c
Kirti Girdhar <kirtigirdhar2004@gmail.com>
2026-07-17 11:21:26 +0530

    Add user registration serializer

--- task_manager/users/serializers.py
+++ task_manager/users/serializers.py
@@ -6,3 +6,16 @@ class UserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['id','username','email']
 
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
