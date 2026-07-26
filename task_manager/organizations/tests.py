045754b55426f65348e7ca367534b8cb7b3d2990
Kirti Girdhar <kirtigirdhar2004@gmail.com>
2026-07-26 13:46:16 +0530

    test for orginizations api

--- task_manager/organizations/tests.py
+++ task_manager/organizations/tests.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import Organization
+
+
+User = get_user_model()
+
+
+class OrganizationTests(APITestCase):
+
+    def setUp(self):
+
+        self.user = User.objects.create_user(
+            username="kirti",
+            password="test123"
+        )
+
+        self.client.force_authenticate(
+            self.user
+        )
+
+    def test_create_organization(self):
+
+        data = {
+            "name": "ABC Software",
+            "description": "Testing organization"
+        }
+
+        response = self.client.post(
+            "/api/organizations/",
+            data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+        self.assertEqual(
+            Organization.objects.count(),
+            1
+        )
+
+        self.assertEqual(
+            Organization.objects.first().owner,
+            self.user
+        )
